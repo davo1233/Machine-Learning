03e0d33366e19768970682fa3184f057a8fe3444
Spenser Tan <51882646+davo1233@users.noreply.github.com>
2022-10-17 21:57:54 +1100

    dsfsdf

--- anbn.py
+++ anbn.py
@@ -54,7 +54,7 @@ class lang_anbn:
 
     def get_sequence(self):
         seq_raw, prob, state = self.get_one_example()
-        seq = torch.from_numpy(np.asarray(seq_raw))
+        seq = torch.from_numpy(np.asarray(seq_raw)).long()
         input = F.one_hot(seq[0:-1],num_classes=self.num_class).float()
         target = torch.from_numpy(np.asarray(prob)).float()
         input = input.unsqueeze(0)
